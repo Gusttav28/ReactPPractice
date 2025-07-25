03b95e57b12cc98ba66031b2a3f68f743ad7b08a
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-07-24 21:14:08 -0600

    second commit

--- projects/helloworld/src/App.jsx
+++ projects/helloworld/src/App.jsx
@@ -3,18 +3,43 @@
 import { TwitterCard } from "./TwitterFollowCard";
 
 export function App() {
-    const gustavo = {isFollowing: true, userName:'Gusttav'}
+    const users = [
+        {
+            userName:'MiduDev',
+            name:'Miguel',
+            isFollowing:true
+        },
+        {
+            userName:'Gusttav',
+            name:'Gustavo',
+            isFollowing:true
+        },
+        {
+            userName:'Dani',
+            name:'Dani',
+            isFollowing:false
+        },
+        {
+            userName:'Juan',
+            name:'Juan Luis',
+            isFollowing:false
+        },
+    ]
     return(
         <>
-            <TwitterCard isFollowing userName="MiduDev">
-                Miguel
-            </TwitterCard>
-            <TwitterCard  {... gustavo}>
-                Gustavo
-            </TwitterCard>
-            <TwitterCard  isFollowing={false} userName="Dani" >
-                Dani
-            </TwitterCard>
+            <section>
+                {
+                    users.map(user => {
+                        const {userName, name, isFollowing} = user
+                        return(
+                            <TwitterCard key={userName} userName={userName} isFollowing={isFollowing}>
+                                {name}
+                            </TwitterCard>
+                            
+                        )
+                    })
+                }
+            </section>
         </>
     )
     
